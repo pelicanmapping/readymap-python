96612272812f1b496330aa25042c8335bd4d8589
Jason Beverage <jasonbeverage@gmail.com>
2014-10-08 20:25:34 +0000

    More tests

--- readymap/tests/test_layer.py
+++ readymap/tests/test_layer.py
@@ -7,20 +7,20 @@ class TestLayer(unittest.TestCase):
         # Login
         self.rm = ReadyMap("http://localhost:8000", username="readymap", password="readymap")
 
-    def test_create(self):
-        layer = self.rm.create_layer({
+    def create_layer(self, name):
+        return self.rm.create_layer({
         "apply_742_colorization": False,
         "apply_background_color": False,
         "background_color": "",
         "data_type": 1,
-        "description": "dd",
+        "description": "",
         "enabled": True,
         "format": "png",
         "generate_tiles_on_demand": True,
         "make_similar_background_colors_transparent": False,
         "max_level": 19,
         "metatile": False,
-        "name": "My Layer 2",
+        "name": "My Layer",
         "public": True,
         "resample": 0,
         "spherical_mercator": False,
@@ -29,7 +29,31 @@ class TestLayer(unittest.TestCase):
         "layer_type": "local"
         })
 
-        print layer.id
+    def test_create_delete(self):
+        # Create a layer
+        layer = self.create_layer("My new layer")
+        self.assertTrue(hasattr(layer, "id"))
+
+        # List all the layers and make sure our new layer is in there
+        layers = self.rm.get_layers()
+
+        found = False
+        for l in layers:
+            if l.id == layer.id:
+                found = True
+                break
+        self.assertTrue(found)
+
+        # Now modify the layer
+        layer.name = "Taco"
+        layer.save()
+
+        # Get the layer from the server and make sure the name has changed.
+        newLayer = self.rm.get_layer(id=layer.id)
+        self.assertEqual(layer.name, newLayer.name)
+
+        # Now delete the layer
+        layer.delete()
 
 
 if __name__ == '__main__':
